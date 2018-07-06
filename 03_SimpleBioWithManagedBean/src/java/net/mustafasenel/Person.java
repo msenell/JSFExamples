bd206f0068d32608d7b309ec0eb482b93f227306
Mustafa Şenel <m_senell@hotmail.com>
2018-07-06 17:38:31 +0300

    ManagedBean yeni örnek.

---
+++ 03_SimpleBioWithManagedBean/src/java/net/mustafasenel/Person.java
@@ -0,0 +1,55 @@
+package net.mustafasenel;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class Person 
+{
+
+    public Person() 
+    {
+    }
+    
+    private String name;
+    private String surname;
+    private String experience;
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public String getSurname() 
+    {
+        return surname;
+    }
+
+    public void setSurname(String surname) 
+    {
+        this.surname = surname;
+    }
+
+    public String getExperience() 
+    {
+        return experience;
+    }
+
+    public void setExperience(String experience) 
+    {
+        this.experience = experience;
+    }    
+    
+    public void writeConsole()
+    {
+        System.out.println("Name : " + name);
+        System.out.println("Surname : " + surname);
+        System.out.println("Experience : " + experience);
+    }
+}
